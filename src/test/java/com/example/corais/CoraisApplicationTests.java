745984f1dfa4eb9125fce561a4e060bbf4ef6f33
carlos5veiga <carlos5veiga@gmail.com>
2021-12-27 06:57:05 -0300

    Projeto criado

---
+++ src/test/java/com/example/corais/CoraisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoraisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
